2ebb1198a2f81f3433b18e37641e479edc4b3e9b
Nitishpokhriyal <nitish.pokhriyal@omlogistics.co.in>
2025-03-05 13:02:09 +0530

    changes

--- src/routes/signupRouter.js
+++ src/routes/signupRouter.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const { signup } = require("../controllers/signUp");
+// const express = require("express");
+// const { signup } = require("../controllers/signUp");
 
 
-const routers = express.Router();
+// // const routers = express.Router();
 
-// Signup Route
-routers.post("/signup", signup);
+// // Signup Route
+// routers.post("/signup", signup);
 
-module.exports = routers;
+// module.exports = routers;
